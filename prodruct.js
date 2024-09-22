b56a0e7844f5457dbb1f196f964ca756dde79269
Ivan <miniglez.98@gmail.com>
2024-09-22 20:43:44 +0200

    product.js fished and test in progress

--- prodruct.js
+++ prodruct.js
@@ -1,21 +1,35 @@
 let products = [];
 let currentId = 0;
 
-const addProducts= (newName, newPrice) => {
+const addProduct = (newName, newPrice) => {
+    if(!newName || !newPrice){
+        throw new Error ("Complete the values to add a product")
+    }
+    const index = products.findIndex(element => element.name == newName)
+    console.log(products)
+    console.log(index)
+    if(index !== -1){
+        throw new Error ("Product already exist")
+    }
     const prodruct = {
         id: currentId,
         name: newName,
         price: newPrice
     }
     currentId ++
-    prodructs.push(prodruct)
-    return prodructs
+    products.push(prodruct)
+    return "Successfull adding"
 }
 
 const removeProduct = (idToRemove) => {
+    const index = products.findIndex(product => product.id == idToRemove)
+    if(!index){
+        throw new Error("ID doesn't exist")
+    }
     const newProducts = products.filter(product => product.id != idToRemove)
+    if(!newProducts)
     products = newProducts
-    return products
+    return "Successfull removal"
 }
 
 const getProducts = () => {
@@ -29,6 +43,24 @@ const resetproducts = () => {
 }
 
 const getProduct = (idToFind) => {
-    const prodruct = products.find(product => prodruct.id = idToFind)
+    const prodruct = products.find(element => element.id = idToFind)
+    if(!prodruct){
+        throw new Error("Product doesn't exist")
+    }
     return prodruct
 }
+
+const updateProduct = (idProduct, newName, newPrice) => {
+    const index = products.findIndex(element => element.id = idProduct)
+    if(!index){
+        throw new Error("Product doesn't exist")
+    }
+    products[index] = {
+        ... products[index],
+        name: newName,
+        price: newPrice
+    }
+    return products
+}
+
+module.exports = { resetproducts, addProduct, removeProduct, getProduct, updateProduct, getProducts }
--- product.test.js
+++ product.test.js
@@ -1,5 +1,40 @@
+const { describe } = require("node:test");
 const { resetproducts, addProduct, removeProduct, getProduct, updateProduct} = require("./prodruct.js")
+const prueba = {
+    id: 0,
+    name: "Arroz",
+    price: 1
+}
 
 beforeEach(() => {
     resetproducts();
+})
+
+describe("addProduct", () => {
+    it("Should return success", () => {
+        expect(addProduct(prueba.name, prueba.price)).toBe("Successfull adding")
+    })
+    beforeEach(() => {
+        resetproducts();
+    })
+    it("Should return error", () => {
+        expect(() => addProduct(1).toThrow("Complete the values to add a product"))
+    })
+    it("Should return error because object exist", () => {
+        addProduct(prueba.name, prueba.price)
+        expect(() => addProduct(prueba.name, prueba.price).toThrow("Product already exist"))
+    })
+    beforeEach(() => {
+        resetproducts();
+    })
+})
+
+describe("removeProduct", () => {
+    addProduct(prueba.name, prueba.price)
+    it("Should return error", () => {
+        expect(() => removeProduct(1).toThrow("ID doesn't exist"))
+    })
+    it("Should be return success", () => {
+        expect(removeProduct(0)).toBe("Successfull removal")
+    })
 })
